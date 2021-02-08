a99e096b989f60038647fc1756fe055fe43ad208
JeongHeon Woo <contact@wookingwoo.com>
2021-02-09 01:53:26 +0900

    Make multiplication_table(webpack)

---
+++ multiplication_table/webpack/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development', // 실서비스: production 
+    devtool: 'eval', // 실서비스:  hidden-source-map
+    resolve: {
+        extensions: ['.jsx', '.js'],
+    },
+
+    entry: {
+        app: './client',
+    },
+
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'], // browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                plugins: [],
+            },
+        }],
+    },
+
+
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+    ],
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist'),
+    },
+};
